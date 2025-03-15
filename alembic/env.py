46e50f6f5b939ac334de206a12d1305ddb77d217
Alina Miashalkina <alin.ka@list.ru>
2025-03-15 21:12:17 +0300

    Initialized alembic.

---
+++ alembic/env.py
@@ -0,0 +1,58 @@
+import asyncio
+import os
+from logging.config import fileConfig
+
+from dotenv import load_dotenv
+from sqlalchemy import engine_from_config, pool
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from alembic import context
+
+from db.models import Base
+
+config = context.config
+
+fileConfig(config.config_file_name)
+
+load_dotenv()
+
+DATABASE_URL = "postgresql+asyncpg://{}:{}@{}:{}/{}".format(
+    os.environ["DB_USER"],
+    os.environ["DB_PASSWORD"],
+    os.environ["DB_HOST"],
+    os.environ["DB_PORT"],
+    os.environ["DB_NAME"]
+)
+
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
+
+connectable = create_async_engine(config.get_main_option("sqlalchemy.url"),
+                                  poolclass=pool.AsyncAdaptedQueuePool)
+
+
+def run_migrations_offline():
+    context.configure(
+        url=DATABASE_URL, target_metadata=Base.metadata, literal_binds=True
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online():
+    async with connectable.connect() as connection:
+        # Измените здесь вызов на do_run_migrations без передачи 'connection'
+        await connection.run_sync(do_run_migrations)
+
+
+def do_run_migrations(connection):
+    context.configure(connection=connection, target_metadata=Base.metadata)
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    import asyncio
+    asyncio.run(run_migrations_online())
